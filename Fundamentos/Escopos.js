81f7d33583db2e894b02726e4186ae35955274a9
Alexandre Pereira <alexandresg19@gmail.com>
2021-01-28 04:16:21 -0300

    Atividade

--- Fundamentos/Escopos.js
+++ Fundamentos/Escopos.js
@@ -1,3 +1,4 @@
+
 function usuario() {
     let nome = 'Alexandre'
     return function() {
@@ -21,3 +22,65 @@ if (true) {
     console.log(youtube);
     console.log(autor);
 }
+
+
+//Closures
+function imprimeNome() {
+    let nome = 'Alexandre';
+    return function() {
+        return nome;
+    }
+}
+
+let func = imprimeNome();
+console.log(func());
+
+function minhaBiblioteca() {
+    function auxiliar(valor) {
+        return valor + 10;
+    }
+
+    return {
+        add5() {
+            return auxiliar(5);
+        },
+        add7() {
+            return auxiliar(7);
+        }
+    }
+};
+
+let result = minhaBiblioteca();
+console.log(result.add5());
+console.log(result.add7());
+
+
+function nomeCompleto(nome) {
+    return function() {
+        console.log(nome, 'Pereira');
+    }
+}
+
+function inicializa() {
+    let nome = 'Alexandre';
+    setTimeout(nomeCompleto(nome), 1000);
+}
+
+inicializa();
+
+
+//Const
+
+const anoNascimento = 1991;
+console.log(anoNascimento)
+
+
+if (true) {
+    const autor = 'Ayrton'
+    console.log(autor);
+}
+
+
+
+
+
